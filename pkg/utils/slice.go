ff7aea32a6fe96de686065a8a678854f077121de
Alexandre Dutra <adutra@users.noreply.github.com>
2021-11-18 23:13:24 +0100

    Remove unused methods

--- pkg/utils/slice.go
+++ pkg/utils/slice.go
@@ -8,25 +8,3 @@ func SliceContains(slice []string, s string) bool {
 	}
 	return false
 }
-
-func SliceRemove(slice []string, s string) []string {
-	newSlice := make([]string, 0)
-	for _, v := range slice {
-		if v != s {
-			newSlice = append(newSlice, s)
-		}
-	}
-	return newSlice
-}
-
-func SlicesContainSameElementsInAnyOrder(slice1, slice2 []string) bool {
-	if len(slice1) != len(slice2) {
-		return false
-	}
-	for _, s := range slice1 {
-		if !SliceContains(slice2, s) {
-			return false
-		}
-	}
-	return true
-}
