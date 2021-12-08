75f26bdce337b4d641abdf122cee40775fdc1e14
Miles-Garnsey <miles.garnsey@datastax.com>
2021-12-08 11:18:20 +1100

    Fix up test names.

--- pkg/telemetry/cass_prometheus_resourcer_test.go
+++ pkg/telemetry/cass_prometheus_resourcer_test.go
@@ -3,22 +3,28 @@
 package telemetry
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-// CassPrometheusResourcer_CreateResources_SUCCESS_Test tests that NewServiceMonitor succeeds in returning a ServiceMonitor.
-func CassPrometheusResourcer_CreateResources_SUCCESS_Test(t *testing.T) {
+// TestCassPrometheusResourcer_UpdateResources_SUCCESS tests that NewServiceMonitor succeeds in creating a ServiceMonitor.
+func TestCassPrometheusResourcer_UpdateResources_SUCCESS(t *testing.T) {
 	assert.Fail(t, "not implemented")
 }
 
-// CassPrometheusResourcer_CleanupResources_FAIL_Incomplete_Test tests that the correct error type is returned when an incomplete
+//TestNewServiceMonitor_SUCCESS tests that a new service monitor is successfully returned.
+func TestNewServiceMonitor_SUCCESS(t *testing.T) {
+	assert.Fail(t, "not implemented")
+}
+
+// TestCassPrometheusResourcer_CleanupResources_FAIL_Incomplete tests that the correct error type is returned when an incomplete
 // CassandraSMConfig is passed to NewServiceMonitor.
-func CassPrometheusResourcer_CleanupResources_FAIL_Incomplete_Test(t *testing.T) {
+func TestCassPrometheusResourcer_CleanupResources_FAIL_Incomplete(t *testing.T) {
 	assert.Fail(t, "not implemented")
 }
 
-// CassPrometheusResourcer_IS_Resourcer_Test tests that CassPrometheusResourcer implements the Resourcer interface.
-func CassPrometheusResourcer_IS_TelemetryResourcer_Test(t *testing.T) {
+// TestCassPrometheusResourcer_IS_TelemetryResourcer tests that CassPrometheusResourcer implements the Resourcer interface.
+func TestCassPrometheusResourcer_IS_TelemetryResourcer(t *testing.T) {
 	assert.Fail(t, "This should be implemented in an envtest probably")
 }
--- pkg/telemetry/cass_telemetry_resourcer_test.go
+++ pkg/telemetry/cass_telemetry_resourcer_test.go
@@ -3,22 +3,23 @@
 package telemetry
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-// CassTelemetryResourcer_CreateResources_SUCCESS_Test tests that NewServiceMonitor succeeds in returning a ServiceMonitor.
-func CassTelemetryResourcer_CreateResources_SUCCESS_Test(t *testing.T) {
+// TestCassTelemetryResourcer_CreateResources_SUCCESS tests that NewServiceMonitor succeeds in creating the expected resources.
+func TestCassTelemetryResourcer_CreateResources_SUCCESS(t *testing.T) {
 	assert.Fail(t, "not implemented")
 }
 
-// CassTelemetryResourcer_CleanupResources_FAIL_Incomplete_Test tests that the correct error type is returned when an incomplete
+// TestCassTelemetryResourcer_CleanupResources_FAIL_Incomplete tests that the correct error type is returned when an incomplete
 // CassandraSMConfig is passed to NewServiceMonitor.
-func CassTelemetryResourcer_CleanupResources_FAIL_Incomplete_Test(t *testing.T) {
+func TestCassTelemetryResourcer_CleanupResources_FAIL_Incomplete(t *testing.T) {
 	assert.Fail(t, "not implemented")
 }
 
-// CassTelemetryResourcer_IS_Resourcer_Test tests that CassTelemetryResourcer implements the Resourcer interface.
-func CassTelemetryResourcer_IS_TelemetryResourcer_Test(t *testing.T) {
+// TestCassTelemetryResourcer_IS_TelemetryResourcer tests that CassTelemetryResourcer implements the Resourcer interface.
+func TestCassTelemetryResourcer_IS_TelemetryResourcer(t *testing.T) {
 	assert.Fail(t, "This should be implemented in an envtest probably")
 }
