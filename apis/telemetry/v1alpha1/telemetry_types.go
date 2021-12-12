7530ed2838a2c72d75fb69acd0bd5d21a1405a0b
Miles-Garnsey <miles.garnsey@datastax.com>
2021-12-13 09:04:34 +1100

    Remove status APIs and logic.

--- apis/telemetry/v1alpha1/telemetry_types.go
+++ apis/telemetry/v1alpha1/telemetry_types.go
@@ -1,11 +1,6 @@
-// Types in this package are instantiated in the other types in k8ssandra-operator, especially Stargate types and Cassandra types.
+// package v1alpha1: Types in this package are instantiated in the other types in k8ssandra-operator, especially Stargate types and Cassandra types.
 //+kubebuilder:object:generate=true
 package v1alpha1
-
-import (
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-)
 
 type TelemetrySpec struct {
 	Prometheus *PrometheusTelemetrySpec `json:"prometheus,omitempty"`
@@ -17,22 +12,3 @@ type PrometheusTelemetrySpec struct {
 	// +optional
 	CommonLabels map[string]string `json:"commonLabels,omitempty"`
 }
-
-type TelemetryStatus struct {
-	// +optional
-	Conditions []TelemetryCondition `json:"conditions,omitempty"`
-}
-type TelemetryCondition struct {
-	Type   TelemetryConditionType `json:"type"`
-	Status corev1.ConditionStatus `json:"status"`
-	// LastTransitionTime is the last time the condition transited from one status to another.
-	// +optional
-	LastTransitionTime *metav1.Time `json:"lastTransitionTime,omitempty"`
-}
-
-type TelemetryConditionType string
-
-const (
-	TelemetryReady  TelemetryConditionType = "Ready"
-	DependencyError TelemetryConditionType = "DependencyError"
-)
