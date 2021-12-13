f79fbe7e326e1ffe1dd17a2979742da0a3f26bfd
Miles-Garnsey <miles.garnsey@datastax.com>
2021-12-13 17:19:32 +1100

    Refactor validation functions. Add testing to validation functions. Add default case to squash bugs in validation functions.

---
+++ pkg/telemetry/validation_test.go
@@ -0,0 +1,32 @@
+package telemetry
+
+import (
+	telemetryapi "github.com/k8ssandra/k8ssandra-operator/apis/telemetry/v1alpha1"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// Test_IsValid_Valid tests that the spec is evaluated as valid when... it is valid.
+func Test_SpecIsValid_Valid(t *testing.T) {
+	spec := telemetryapi.TelemetrySpec{Prometheus: &telemetryapi.PrometheusTelemetrySpec{
+		Enabled: false,
+	}}
+	isValid, err := SpecIsValid(&spec, false)
+	if err != nil {
+		assert.Fail(t, "couldn't determine if spec was valid or not", err)
+	}
+	assert.True(t, isValid)
+}
+
+// Test_IsValid_Valid tests that the spec is evaluated as valid when... it is not valid.
+func Test_SpecIsValid_NotValid(t *testing.T) {
+	spec := telemetryapi.TelemetrySpec{Prometheus: &telemetryapi.PrometheusTelemetrySpec{
+		Enabled: true,
+	}}
+	isValid, err := SpecIsValid(&spec, false)
+	if err != nil {
+		assert.Fail(t, "couldn't determine if spec was valid or not", err)
+	}
+	assert.False(t, isValid)
+
+}
