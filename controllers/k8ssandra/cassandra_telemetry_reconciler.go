ba65fbd82f46b34f00947f1dabdd4b8677cb0be2
Miles-Garnsey <miles.garnsey@datastax.com>
2021-12-18 15:24:33 +1100

    Use `result.Continue()` throughout telemetry reconciler, so that the reconciliation proceeds rather than returning too early.

--- controllers/k8ssandra/cassandra_telemetry_reconciler.go
+++ controllers/k8ssandra/cassandra_telemetry_reconciler.go
@@ -45,7 +45,7 @@ func (r *K8ssandraClusterReconciler) reconcileCassandraDCTelemetry(
 	}
 	// If Prometheus not installed bail here.
 	if !promInstalled {
-		return result.Done()
+		return result.Continue()
 	}
 	// Determine if we want a cleanup or a resource update.
 	switch {
@@ -54,25 +54,25 @@ func (r *K8ssandraClusterReconciler) reconcileCassandraDCTelemetry(
 		if err := dcCfg.CleanupResources(ctx, remoteClient); err != nil {
 			return result.Error(err)
 		}
-		return result.Done()
+		return result.Continue()
 	case mergedSpec.Prometheus == nil:
 		logger.Info("Telemetry not present for CassDC, will delete resources", "mergedSpec", mergedSpec)
 		if err := dcCfg.CleanupResources(ctx, remoteClient); err != nil {
 			return result.Error(err)
 		}
-		return result.Done()
+		return result.Continue()
 	case mergedSpec.Prometheus.Enabled:
 		logger.Info("Prometheus config found", "mergedSpec", mergedSpec)
 		dcCfg.TelemetrySpec = mergedSpec
 		if err := dcCfg.UpdateResources(ctx, remoteClient, actualDc); err != nil {
 			return result.Error(err)
 		}
-		return result.Done()
+		return result.Continue()
 	default:
 		logger.Info("Telemetry not present for CassDC, will delete resources", "mergedSpec", mergedSpec)
 		if err := dcCfg.CleanupResources(ctx, remoteClient); err != nil {
 			return result.Error(err)
 		}
-		return result.Done()
+		return result.Continue()
 	}
 }
