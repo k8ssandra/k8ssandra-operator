62ce6c2550138a431ade179907fb5864ce1247fb
Alexandre Dutra <adutra@users.noreply.github.com>
2022-02-03 18:38:35 -0300

    Tests should verify that replication is not changed when stopping a DC

--- controllers/k8ssandra/stop_dc_test.go
+++ controllers/k8ssandra/stop_dc_test.go
@@ -2,10 +2,14 @@ package k8ssandra
 
 import (
 	"context"
+	"github.com/go-logr/logr"
 	cassdcapi "github.com/k8ssandra/cass-operator/apis/cassandra/v1beta1"
 	api "github.com/k8ssandra/k8ssandra-operator/apis/k8ssandra/v1alpha1"
 	reaperapi "github.com/k8ssandra/k8ssandra-operator/apis/reaper/v1alpha1"
 	stargateapi "github.com/k8ssandra/k8ssandra-operator/apis/stargate/v1alpha1"
+	"github.com/k8ssandra/k8ssandra-operator/pkg/cassandra"
+	"github.com/k8ssandra/k8ssandra-operator/pkg/stargate"
+	testutils "github.com/k8ssandra/k8ssandra-operator/pkg/test"
 	"github.com/k8ssandra/k8ssandra-operator/pkg/utils"
 	"github.com/k8ssandra/k8ssandra-operator/test/framework"
 	"github.com/stretchr/testify/assert"
@@ -31,6 +35,7 @@ func stopDcTest(f *framework.Framework, ctx context.Context, test stopDcTestFunc
 		if err := f.CreateNamespace(namespace); err != nil {
 			t.Fatalf("failed to create namespace %s: %v", namespace, err)
 		}
+		managementApiFactory.Reset()
 		kc := stopDcTestSetup(t, f, ctx, namespace)
 		test(t, f, ctx, kc)
 		err := f.DeleteK8ssandraCluster(ctx, utils.GetKey(kc))
@@ -134,6 +139,29 @@ func stopDcTestSetup(t *testing.T, f *framework.Framework, ctx context.Context, 
 	return kc
 }
 
+// stopDcManagementApiReset resets the mock for the Management API so that it only responds to calls to
+// EnsureKeyspaceReplication that have the desired replication. Other calls with undesired replications would then
+// panic. This ensures that stopping a DC does not modify the desired replication of keyspaces in the cluster.
+func stopDcManagementApiReset(replication map[string]int) {
+	managementApiFactory.Reset()
+	mockMgmtApi := testutils.NewFakeManagementApiFacade()
+	// Only accept calls to EnsureKeyspaceReplication with the desired replication for system keyspaces:
+	for _, keyspace := range api.SystemKeyspaces {
+		mockMgmtApi.On(testutils.EnsureKeyspaceReplication, keyspace, replication).Return(nil)
+	}
+	// Same for Stargate and Reaper keyspaces:
+	mockMgmtApi.On(testutils.EnsureKeyspaceReplication, stargate.AuthKeyspace, replication).Return(nil)
+	mockMgmtApi.On(testutils.EnsureKeyspaceReplication, reaperapi.DefaultKeyspace, replication).Return(nil)
+	// Other stubs that we need to define:
+	mockMgmtApi.On(testutils.GetSchemaVersions).Return(map[string][]string{"version1": {"host1"}, "UNREACHABLE": {"host2"}}, nil)
+	mockMgmtApi.On(testutils.ListTables, stargate.AuthKeyspace).Return([]string{"token"}, nil)
+	mockMgmtApi.On(testutils.ListKeyspaces, "").Return([]string{}, nil)
+	adapter := func(ctx context.Context, datacenter *cassdcapi.CassandraDatacenter, client client.Client, logger logr.Logger) (cassandra.ManagementApiFacade, error) {
+		return mockMgmtApi, nil
+	}
+	managementApiFactory.SetAdapter(adapter)
+}
+
 // stopExistingDc tests the creation of a new K8ssandraCluster containing 2 dcs, dc1 and dc2. dc1 is then stopped. It
 // expects dc1 to be in stopped state, and its Stargate and Reaper resources to be deleted. It expects dc2 to remain
 // deployed and ready at all times, along with its Stargate and Reaper resources.
@@ -145,6 +173,9 @@ func stopExistingDc(t *testing.T, f *framework.Framework, ctx context.Context, k
 	sg2Key := framework.NewClusterKey(k8sCtx1, kc.Namespace, kc.Name+"-dc2-stargate")
 	reaper1Key := framework.NewClusterKey(k8sCtx0, kc.Namespace, kc.Name+"-dc1-reaper")
 	reaper2Key := framework.NewClusterKey(k8sCtx1, kc.Namespace, kc.Name+"-dc2-reaper")
+
+	replication := map[string]int{"dc1": 3, "dc2": 3}
+	stopDcManagementApiReset(replication)
 
 	t.Log("stop dc1")
 	patch := client.MergeFromWithOptions(kc.DeepCopy(), client.MergeFromWithOptimisticLock{})
@@ -267,6 +298,26 @@ func addAndStopDc(t *testing.T, f *framework.Framework, ctx context.Context, kc 
 	err = f.SetReaperStatusReady(ctx, reaper3Key)
 	require.NoError(t, err, "failed to patch reaper status")
 
+	t.Log("wait for the dc conditions to be met")
+	require.Eventually(t, func() bool {
+		err := f.Client.Get(ctx, kcKey, kc)
+		return assert.NoError(t, err) &&
+			assert.Len(t, kc.Status.Datacenters, 3) &&
+			assert.Equal(t, corev1.ConditionTrue, kc.Status.Datacenters["dc1"].Cassandra.GetConditionStatus(cassdcapi.DatacenterReady)) &&
+			assert.Equal(t, corev1.ConditionTrue, kc.Status.Datacenters["dc2"].Cassandra.GetConditionStatus(cassdcapi.DatacenterReady)) &&
+			assert.Equal(t, corev1.ConditionTrue, kc.Status.Datacenters["dc3"].Cassandra.GetConditionStatus(cassdcapi.DatacenterReady)) &&
+			assert.Equal(t, corev1.ConditionTrue, kc.Status.Datacenters["dc1"].Stargate.GetConditionStatus(stargateapi.StargateReady)) &&
+			assert.Equal(t, corev1.ConditionTrue, kc.Status.Datacenters["dc2"].Stargate.GetConditionStatus(stargateapi.StargateReady)) &&
+			assert.Equal(t, corev1.ConditionTrue, kc.Status.Datacenters["dc3"].Stargate.GetConditionStatus(stargateapi.StargateReady)) &&
+			assert.Equal(t, corev1.ConditionTrue, kc.Status.Datacenters["dc1"].Reaper.GetConditionStatus(reaperapi.ReaperReady)) &&
+			assert.Equal(t, corev1.ConditionTrue, kc.Status.Datacenters["dc2"].Reaper.GetConditionStatus(reaperapi.ReaperReady)) &&
+			assert.Equal(t, corev1.ConditionTrue, kc.Status.Datacenters["dc3"].Reaper.GetConditionStatus(reaperapi.ReaperReady))
+	}, timeout, interval, "timed out waiting for dc condition check")
+
+	// From now on, only expect the updated replication including dc3 to be enforced on all keyspaces
+	replication := map[string]int{"dc1": 3, "dc2": 3, "dc3": 3}
+	stopDcManagementApiReset(replication)
+
 	t.Log("stop dc3")
 	err = f.Client.Get(ctx, kcKey, kc)
 	require.NoError(t, err)
@@ -358,5 +409,4 @@ func addAndStopDc(t *testing.T, f *framework.Framework, ctx context.Context, kc 
 	t.Logf("update reaper reaper3 status to ready")
 	err = f.SetReaperStatusReady(ctx, reaper3Key)
 	require.NoError(t, err, "failed to patch reaper status")
-
 }
