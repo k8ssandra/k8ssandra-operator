b0c223e299721e970c6cd12283c1ccee9016c144
Miles-Garnsey <miles.garnsey@datastax.com>
2022-02-07 09:44:24 +1100

    Revert changes to tests to assume that cass-operator always installed into test namespace, never `cass-operator`.

--- test/e2e/suite_test.go
+++ test/e2e/suite_test.go
@@ -283,11 +283,9 @@ func getTestFixtureDir(fixture TestFixture) (string, error) {
 // required by both operators.
 func beforeTest(t *testing.T, f *framework.E2eFramework, fixtureDir string, opts *e2eTestOpts) error {
 	namespaces := make([]string, 0)
-	cassOperatorNS := opts.sutNamespace
 
 	if opts.clusterScoped {
 		namespaces = append(namespaces, opts.sutNamespace)
-		cassOperatorNS = "cass-operator"
 	}
 
 	namespaces = append(namespaces, opts.operatorNamespace)
@@ -344,7 +342,7 @@ func beforeTest(t *testing.T, f *framework.E2eFramework, fixtureDir string, opts
 		return err
 	}
 
-	if err := f.WaitForCassOperatorToBeReady(cassOperatorNS, polling.operatorDeploymentReady.timeout, polling.operatorDeploymentReady.interval); err != nil {
+	if err := f.WaitForCassOperatorToBeReady(opts.sutNamespace, polling.operatorDeploymentReady.timeout, polling.operatorDeploymentReady.interval); err != nil {
 		t.Log("failed waiting for cass-operator to be ready")
 		return err
 	}
