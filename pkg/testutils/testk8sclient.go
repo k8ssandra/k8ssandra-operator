bedd34d083c2a502b9d09ecc1c5547b94323ac49
Miles-Garnsey <miles.garnsey@datastax.com>
2022-02-15 08:56:00 +1100

    Better async `Get()`.

--- pkg/testutils/testk8sclient.go
+++ pkg/testutils/testk8sclient.go
@@ -2,6 +2,7 @@ package testutils
 
 import (
 	"context"
+	"sync"
 	"testing"
 	"time"
 
@@ -17,49 +18,33 @@ import (
 
 type TestK8sClient struct {
 	client.Client
-	TestState     *testing.T
-	UnsafeGetSync func(ctx context.Context, key client.ObjectKey, obj client.Object) error
+	TestState      *testing.T
+	UnsafeGetSync  func(ctx context.Context, key client.ObjectKey, obj client.Object) error
 	UnsafeListSync func(ctx context.Context, list client.ObjectList, opts ...client.ListOption) error
-	timeout       time.Duration
-	tick          time.Duration
+	timeout        time.Duration
+	tick           time.Duration
 }
 
-// SleepWithContext thanks to [this](https://stackoverflow.com/a/69291047) SO answer.
-func SleepWithContext(ctx context.Context, d time.Duration) {
-	timer := time.NewTimer(d)
-	select {
-	case <-ctx.Done():
-		if !timer.Stop() {
-			<-timer.C
-		}
-	case <-timer.C:
-	}
-}
-
-// Get IS AN ASYNC SAFE client.Get(), it is not the normal `Get)`! Use UnsafeListSync() if you want regular Get()'s behaviour.
+// Get IS AN ASYNC SAFE client.Get(), it is not the normal `Get()`! Use UnsafeListSync() if you want regular Get()'s behaviour.
 func (my TestK8sClient) Get(ctx context.Context, key client.ObjectKey, obj client.Object) error {
 	var err error
-	ticker := time.NewTicker(my.tick)
-	sleepCtx, sleepCancel := context.WithCancel(context.Background())
-	done := make(chan bool)
-	defer sleepCancel()
-	go func(){
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() error {
+		defer wg.Done()
 		for {
 			select {
-			case <-done:
-				return
-			case <-ticker.C:
+			case <-time.After(my.tick): // Poll
 				err = my.UnsafeGetSync(ctx, key, obj)
 				if err == nil {
-					sleepCancel()
-					return
+					return nil
+				}
+			case <-time.After(my.timeout): // Timeout
+				return err
 			}
 		}
-		}
 	}()
-	SleepWithContext(sleepCtx, my.timeout)
-	ticker.Stop()
-	done <- true
+	wg.Wait()
 	return err
 }
 
@@ -84,12 +69,12 @@ func NewTestk8sClient(t *testing.T, configuredClient client.Client, timeout time
 	utilruntime.Must(reaperapi.AddToScheme(testScheme))
 	utilruntime.Must(stargateapi.AddToScheme(testScheme))
 	testClient := TestK8sClient{
-		Client:        configuredClient,
-		TestState:     t,
-		UnsafeGetSync: configuredClient.Get,
+		Client:         configuredClient,
+		TestState:      t,
+		UnsafeGetSync:  configuredClient.Get,
 		UnsafeListSync: configuredClient.List,
-		timeout:       timeout,
-		tick:          tick,
+		timeout:        timeout,
+		tick:           tick,
 	}
 	return testClient
 }
