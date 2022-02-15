3d8d851514b98db39e08e4dc00255a09c450de5f
Michael Burman <yak@iki.fi>
2022-02-15 15:35:27 +0200

    Fix e2e framework replacements not matching other kustomize rules

--- test/framework/e2e_framework.go
+++ test/framework/e2e_framework.go
@@ -208,6 +208,16 @@ replacements:
       kind: ClusterRoleBinding
     fieldPaths:
       - subjects.0.namespace
+  - select:
+      name: cass-operator-validating-webhook-configuration
+      kind: ValidatingWebhookConfiguration
+    fieldPaths:
+      - webhooks.0.clientConfig.service.namespace
+  - select:
+      name: k8ssandra-operator-validating-webhook-configuration
+      kind: ValidatingWebhookConfiguration
+    fieldPaths:
+      - webhooks.0.clientConfig.service.namespace
 `
 
 	dataPlaneTmpl := `
@@ -249,42 +259,16 @@ replacements:
       kind: ClusterRoleBinding
     fieldPaths:
       - subjects.0.namespace
-replacements:
-- source: 
-    kind: Namespace
-    name: {{ .Namespace }}
-    fieldPath: metadata.name
-  targets:
   - select:
-      namespace: cass-operator
+      name: cass-operator-validating-webhook-configuration
+      kind: ValidatingWebhookConfiguration
     fieldPaths:
-      - metadata.namespace
+      - webhooks.0.clientConfig.service.namespace
   - select:
-      namespace: k8ssandra-operator
+      name: k8ssandra-operator-validating-webhook-configuration
+      kind: ValidatingWebhookConfiguration
     fieldPaths:
-      - metadata.namespace
-  - select:
-      kind: ClusterRoleBinding
-    fieldPaths:
-      - subjects.0.namespace
-replacements:
-- source: 
-    kind: Namespace
-    name: {{ .Namespace }}
-    fieldPath: metadata.name
-  targets:
-  - select:
-      namespace: cass-operator
-    fieldPaths:
-      - metadata.namespace
-  - select:
-      namespace: k8ssandra-operator
-    fieldPaths:
-    - metadata.namespace
-  - select:
-      kind: ClusterRoleBinding
-    fieldPaths:
-    - subjects.0.namespace
+      - webhooks.0.clientConfig.service.namespace
 `
 
 	k := Kustomization{Namespace: config.Namespace, ImageTag: config.ImageTag}
