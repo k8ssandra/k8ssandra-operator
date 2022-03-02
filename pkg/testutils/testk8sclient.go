c6da012a8fe846dcd7207af6d908e03cabc6db63
Miles-Garnsey <miles.garnsey@datastax.com>
2022-03-02 13:35:36 +1100

    Fix List() and remove returns from goroutine.

--- pkg/testutils/testk8sclient.go
+++ pkg/testutils/testk8sclient.go
@@ -11,7 +11,6 @@ import (
 	reaperapi "github.com/k8ssandra/k8ssandra-operator/apis/reaper/v1alpha1"
 	stargateapi "github.com/k8ssandra/k8ssandra-operator/apis/stargate/v1alpha1"
 	promapi "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
-	"github.com/stretchr/testify/assert"
 	"k8s.io/apimachinery/pkg/api/errors"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -53,12 +52,24 @@ func (my TestK8sClient) Get(ctx context.Context, key client.ObjectKey, obj clien
 // List IS AN ASYNC SAFE client.List(), it is not the normal `List()`! Use UnsafeListSync() if you want regular List()'s behaviour.
 func (my TestK8sClient) List(ctx context.Context, list client.ObjectList, opts ...client.ListOption) error {
 	var err error
-	assert.Eventually(my.TestState, func() bool {
-		if err := my.UnsafeListSync(ctx, list, opts...); err != nil {
-			return false
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-time.After(my.tick): // Poll
+				err = my.UnsafeListSync(ctx, list, opts...)
+				if err == nil {
+					return
+				}
+			case <-time.After(my.timeout): // Timeout
+				err = errors.NewTimeoutError("timed out trying to get resource", 0)
+				return
+			}
 		}
-		return true
-	}, my.timeout, my.tick)
+	}()
+	wg.Wait()
 	return err
 }
 
