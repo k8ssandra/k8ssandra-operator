0db1e1559b5e730ea635eb89043e4d42fcd1757c
John Sanda <john.sanda@gmail.com>
2022-03-04 11:44:37 -0500

    fix e2e test

--- test/e2e/suite_test.go
+++ test/e2e/suite_test.go
@@ -805,28 +805,39 @@ func addDcToCluster(t *testing.T, ctx context.Context, namespace string, f *fram
 
 	t.Log("create keyspaces")
 	_, err = f.ExecuteCql(ctx, k8sCtx0, namespace, "test", "test-dc1-default-sts-0",
-		"CREATE KEYSPACE ks1 WITH REPLICATION = {'class' : 'NetworkTopologyStrategy', 'dc1' : 1}")
+		"CREATE KEYSPACE ks1 WITH REPLICATION = {'class' : 'NetworkTopologyStrategy', 'dc1' : 2}")
 	require.NoError(err, "failed to create keyspace")
 
 	_, err = f.ExecuteCql(ctx, k8sCtx0, namespace, "test", "test-dc1-default-sts-0",
-		"CREATE KEYSPACE ks2 WITH REPLICATION = {'class' : 'NetworkTopologyStrategy', 'dc1' : 1}")
+		"CREATE KEYSPACE ks2 WITH REPLICATION = {'class' : 'NetworkTopologyStrategy', 'dc1' : 2}")
 	require.NoError(err, "failed to create keyspace")
 
 	t.Log("add dc2 to cluster")
-	err = f.Client.Get(ctx, kcKey, kc)
-	require.NoError(err, "failed to get K8ssandraCluster %s", kcKey)
+	require.Eventually(func() bool {
+		kc := &api.K8ssandraCluster{}
+		err = f.Client.Get(ctx, kcKey, kc)
+		if err != nil {
+			t.Logf("failed to add DC: failed to get K8ssandraCluster: %v", err)
+			return false
+		}
 
-	kc.Spec.Cassandra.Datacenters = append(kc.Spec.Cassandra.Datacenters, api.CassandraDatacenterTemplate{
-		Meta: api.EmbeddedObjectMeta{
-			Name: "dc2",
-		},
-		K8sContext: k8sCtx1,
-		Size:       1,
-	})
-	annotations.AddAnnotation(kc, api.DcReplicationAnnotation, `{"dc2": {"ks1": 1, "ks2": 1}}`)
+		kc.Spec.Cassandra.Datacenters = append(kc.Spec.Cassandra.Datacenters, api.CassandraDatacenterTemplate{
+			Meta: api.EmbeddedObjectMeta{
+				Name: "dc2",
+			},
+			K8sContext: k8sCtx1,
+			Size:       2,
+		})
+		annotations.AddAnnotation(kc, api.DcReplicationAnnotation, `{"dc2": {"ks1": 2, "ks2": 2}}`)
 
-	err = f.Client.Update(ctx, kc)
-	require.NoError(err, "failed to update K8ssandraCluster")
+		err = f.Client.Update(ctx, kc)
+		if err != nil {
+			t.Logf("failed to add DC: %v", err)
+			return false
+		}
+
+		return true
+	}, 30 * time.Second, 1 * time.Second, "timed out waiting to add DC to K8ssandraCluster")
 
 	dc2Key := framework.ClusterKey{K8sContext: "kind-k8ssandra-1", NamespacedName: types.NamespacedName{Namespace: namespace, Name: "dc2"}}
 	checkDatacenterReady(t, ctx, dc2Key, f)
@@ -837,7 +848,7 @@ func addDcToCluster(t *testing.T, ctx context.Context, namespace string, f *fram
 
 	t.Log("check that nodes in dc1 see nodes in dc2")
 	pod := "test-dc1-default-sts-0"
-	count := 2
+	count := 4
 	checkNodeToolStatus(t, f, "kind-k8ssandra-0", namespace, pod, count, 0, "-u", username, "-pw", password)
 
 	assert.NoError(err, "timed out waiting for nodetool status check against "+pod)
@@ -857,7 +868,7 @@ func addDcToCluster(t *testing.T, ctx context.Context, namespace string, f *fram
 				t.Logf("replication check for keyspace %s failed: %v", ks, err)
 				return false
 			}
-			return strings.Contains(output, "'dc1': '1'") && strings.Contains(output, "'dc2': '1'")
+			return strings.Contains(output, "'dc1': '2'") && strings.Contains(output, "'dc2': '2'")
 		}, 1*time.Minute, 5*time.Second, "failed to verify replication updated for keyspace %s", ks)
 	}
 
