301148029b4b05f138e3d50ea774b88b05221e5e
Miles-Garnsey <miles.garnsey@datastax.com>
2022-04-14 17:18:32 +1000

    Add unit test `TestGetSourceRacksIPs()`.

--- pkg/medusa/hostmap_test.go
+++ pkg/medusa/hostmap_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"testing"
 
+	cassdcapi "github.com/k8ssandra/cass-operator/apis/cassandra/v1beta1"
+	k8ssandraapi "github.com/k8ssandra/k8ssandra-operator/apis/k8ssandra/v1alpha1"
 	pkgtest "github.com/k8ssandra/k8ssandra-operator/pkg/test"
 	"github.com/stretchr/testify/assert"
 	"inet.af/netaddr"
@@ -64,3 +66,39 @@ func TestGetSourceRacksIPs(t *testing.T) {
 	}
 	assert.Equal(t, expectedSourceRacks, sourceRacks)
 }
+
+func TestGetTargetRackFQDNs(t *testing.T) {
+	kluster := pkgtest.NewK8ssandraCluster("test-cluster", "default")
+	kluster.Spec.Cassandra.Datacenters = []k8ssandraapi.CassandraDatacenterTemplate{
+		{
+			Meta: k8ssandraapi.EmbeddedObjectMeta{
+				Name:      "test-dc2",
+				Namespace: "default",
+			},
+			Size: 3,
+		},
+	}
+	result, err := getTargetRackFQDNs(kluster, "test-dc2")
+	assert.NoError(t, err, err)
+	expectedSourceRacks := map[NodeLocation][]HostName{
+		{Rack: "default", DC: "test-dc2"}: {HostName("test-cluster-test-dc2-default-sts-0"), HostName("test-cluster-test-dc2-default-sts-1"), HostName("test-cluster-test-dc2-default-sts-2")},
+	}
+	assert.Equal(t, expectedSourceRacks, result)
+	kluster.Spec.Cassandra.Racks = []cassdcapi.Rack{
+		{Name: "rack1"},
+		{Name: "rack2"},
+		{Name: "rack3"},
+	}
+	expectedSourceRacks = map[NodeLocation][]HostName{
+		{Rack: "rack1", DC: "test-dc2"}: {HostName("test-cluster-test-dc2-rack1-sts-0")},
+		{Rack: "rack2", DC: "test-dc2"}: {HostName("test-cluster-test-dc2-rack2-sts-0")},
+		{Rack: "rack3", DC: "test-dc2"}: {HostName("test-cluster-test-dc2-rack3-sts-0")},
+	}
+	result, err = getTargetRackFQDNs(kluster, "test-dc2")
+	assert.NoError(t, err, err)
+	assert.Equal(t, expectedSourceRacks, result)
+}
+
+// func TestGetHostMap(t *testing.T) {
+
+// }
