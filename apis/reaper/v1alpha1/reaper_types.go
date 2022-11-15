7115ecea07206da5cf2bd126e2a8164e05c5538c
Olivier Michallat <olim7t@users.noreply.github.com>
2022-11-15 09:16:07 -0800

    Revert deprecation annotation

--- apis/reaper/v1alpha1/reaper_types.go
+++ apis/reaper/v1alpha1/reaper_types.go
@@ -49,9 +49,8 @@ type ReaperTemplate struct {
 	// +optional
 	CassandraUserSecretRef corev1.LocalObjectReference `json:"cassandraUserSecretRef,omitempty"`
 
-	// Defines the username and password that Reaper will use to authenticate JMX connections to Cassandra clusters.
-	// +kubebuilder:deprecatedversion:warning="JMX security is now based on CQL roles. Reaper will use CassandraUsefSecretRef for authentication, JmxUserSecretRef is now ignored."
-	// +optional
+	// Deprecated: JMX security is now based on CQL roles. Reaper will use cassandraUsefSecretRef for authentication,
+	// this field is ignored.
 	JmxUserSecretRef corev1.LocalObjectReference `json:"jmxUserSecretRef,omitempty"`
 
 	// Defines the secret which contains the username and password for the Reaper UI and REST API authentication.
