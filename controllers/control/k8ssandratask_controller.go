ee65fe2c65efdfbc5129f4bfe99c7c2240429e2e
Olivier Michallat <olim7t@users.noreply.github.com>
2022-12-02 10:21:12 -0800

    Revisit naming and add a few comments

--- controllers/control/k8ssandratask_controller.go
+++ controllers/control/k8ssandratask_controller.go
@@ -58,7 +58,10 @@ var (
 	noTTL int32 = 0
 )
 
-// K8ssandraTaskReconciler reconciles a K8ssandraTask object
+// K8ssandraTaskReconciler reconciles a K8ssandraTask object:
+//   - create a CassandraTask for each DC specified by the K8ssandraTask. Those will be picked up and executed by
+//     cass-operator.
+//   - watch the CassandraTasks (see SetupWithManager), and update the K8ssandraTask's status accordingly.
 type K8ssandraTaskReconciler struct {
 	*config.ReconcilerConfig
 	client.Client
@@ -78,48 +81,48 @@ func (r *K8ssandraTaskReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 	logger := log.FromContext(ctx).WithValues("K8ssandraTask", req.NamespacedName)
 
 	logger.Info("Fetching task")
-	k8Task := &api.K8ssandraTask{}
-	if err := r.Get(ctx, req.NamespacedName, k8Task); err != nil {
+	kTask := &api.K8ssandraTask{}
+	if err := r.Get(ctx, req.NamespacedName, kTask); err != nil {
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
-	kcKey := k8Task.GetClusterKey()
+	kcKey := kTask.GetClusterKey()
 	kc, kcExists, err := r.getCluster(ctx, kcKey)
 	if err != nil {
 		return ctrl.Result{}, err
 	}
 
 	// Set owner reference so that the task is deleted when the K8ssandraCluster is deleted.
-	if k8Task.OwnerReferences == nil && kcExists {
+	if kTask.OwnerReferences == nil && kcExists {
 		logger.Info("Setting owner reference", "K8ssandraCluster", kcKey)
-		if err := controllerutil.SetControllerReference(kc, k8Task, r.Scheme); err != nil {
+		if err := controllerutil.SetControllerReference(kc, kTask, r.Scheme); err != nil {
 			return ctrl.Result{}, fmt.Errorf("setting owner reference on K8ssandraTask: %s", err)
 		}
-		if err := r.Update(ctx, k8Task); err != nil {
+		if err := r.Update(ctx, kTask); err != nil {
 			return ctrl.Result{}, fmt.Errorf("updating K8ssandraTask to set owner reference: %s", err)
 		}
 		return ctrl.Result{Requeue: true}, nil
 	}
 
 	// Handle deletion
-	if k8Task.ObjectMeta.DeletionTimestamp.IsZero() {
-		if !controllerutil.ContainsFinalizer(k8Task, k8ssandraTaskFinalizer) {
+	if kTask.ObjectMeta.DeletionTimestamp.IsZero() {
+		if !controllerutil.ContainsFinalizer(kTask, k8ssandraTaskFinalizer) {
 			logger.Info("Adding finalizer")
-			controllerutil.AddFinalizer(k8Task, k8ssandraTaskFinalizer)
-			if err := r.Update(ctx, k8Task); err != nil {
+			controllerutil.AddFinalizer(kTask, k8ssandraTaskFinalizer)
+			if err := r.Update(ctx, kTask); err != nil {
 				return ctrl.Result{}, err
 			}
 			return ctrl.Result{Requeue: true}, nil
 		}
 	} else { // The task is being deleted
-		if controllerutil.ContainsFinalizer(k8Task, k8ssandraTaskFinalizer) {
+		if controllerutil.ContainsFinalizer(kTask, k8ssandraTaskFinalizer) {
 			// First time we've noticed the deletion, clean up dependents and remove the finalizer
-			if err := r.deleteDcTasks(ctx, k8Task, kc, logger); err != nil {
+			if err := r.deleteCassandraTasks(ctx, kTask, kc, logger); err != nil {
 				return ctrl.Result{}, err
 			}
 			logger.Info("Removing finalizer")
-			controllerutil.RemoveFinalizer(k8Task, k8ssandraTaskFinalizer)
-			if err := r.Update(ctx, k8Task); err != nil {
+			controllerutil.RemoveFinalizer(kTask, k8ssandraTaskFinalizer)
+			if err := r.Update(ctx, kTask); err != nil {
 				return ctrl.Result{}, err
 			}
 		}
@@ -127,70 +130,70 @@ func (r *K8ssandraTaskReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 	}
 
 	// Handle TTL
-	if k8Task.Status.CompletionTime != nil {
+	if kTask.Status.CompletionTime != nil {
 		timeNow := metav1.Now()
-		deletionTime := calculateDeletionTime(k8Task)
+		deletionTime := calculateDeletionTime(kTask)
 		if deletionTime.IsZero() { // No TTL set, we're done
 			return ctrl.Result{}, nil
 		}
 
 		if deletionTime.Before(timeNow.Time) {
-			logger.Info("Deleting task due to expired TTL")
-			err := r.Delete(ctx, k8Task)
+			logger.Info("Deleting K8ssandraTask due to expired TTL")
+			err := r.Delete(ctx, kTask)
 			return ctrl.Result{}, client.IgnoreNotFound(err)
 		}
 
 		// Reschedule for later deletion
 		nextRunTime := deletionTime.Sub(timeNow.Time)
-		logger.Info("Scheduling task deletion", "Delay", nextRunTime)
+		logger.Info("Scheduling K8ssandraTask deletion", "Delay", nextRunTime)
 		return ctrl.Result{RequeueAfter: nextRunTime}, nil
 	}
 
-	// Create subtasks and/or gather their status
+	// Create CassandraTasks and/or gather their status
 	if !kcExists {
-		return r.reportInvalidSpec(ctx, k8Task, "Unknown K8ssandraCluster %s.%s", kcKey.Namespace, kcKey.Name)
+		return r.reportInvalidSpec(ctx, kTask, "Unknown K8ssandraCluster %s.%s", kcKey.Namespace, kcKey.Name)
 	}
-	patch := client.MergeFrom(k8Task.DeepCopy())
-	if dcs, err := filterDcs(kc, k8Task.Spec.Datacenters); err != nil {
-		return r.reportInvalidSpec(ctx, k8Task, err.Error())
+	patch := client.MergeFrom(kTask.DeepCopy())
+	if dcs, err := filterDcs(kc, kTask.Spec.Datacenters); err != nil {
+		return r.reportInvalidSpec(ctx, kTask, err.Error())
 	} else {
 		for _, dc := range dcs {
 			dcNamespace := utils.FirstNonEmptyString(dc.Meta.Namespace, kc.Namespace)
-			desiredDcTask := newDcTask(k8Task, dcNamespace, dc.Meta.Name)
+			desiredCTask := newCassandraTask(kTask, dcNamespace, dc.Meta.Name)
 
 			remoteClient, err := r.ClientCache.GetRemoteClient(dc.K8sContext)
 			if err != nil {
 				return ctrl.Result{}, err
 			}
 
-			dcTaskKey := client.ObjectKeyFromObject(desiredDcTask)
-			actualDcTask := &cassapi.CassandraTask{}
-			if err = remoteClient.Get(ctx, dcTaskKey, actualDcTask); err != nil {
+			cTaskKey := client.ObjectKeyFromObject(desiredCTask)
+			actualCTask := &cassapi.CassandraTask{}
+			if err = remoteClient.Get(ctx, cTaskKey, actualCTask); err != nil {
 				if errors.IsNotFound(err) {
-					if err = remoteClient.Create(ctx, desiredDcTask); err != nil {
+					if err = remoteClient.Create(ctx, desiredCTask); err != nil {
 						return ctrl.Result{}, err
 					}
-					r.recordDcTaskCreated(k8Task, desiredDcTask, dc.K8sContext)
+					r.recordCassandraTaskCreated(kTask, desiredCTask, dc.K8sContext)
 				} else {
 					return ctrl.Result{}, err
 				}
 			} else {
-				logger.Info("DC task already exists, retrieving its status", "CassandraTask", dcTaskKey)
-				if k8Task.Status.Datacenters == nil {
-					k8Task.Status.Datacenters = make(map[string]cassapi.CassandraTaskStatus)
+				logger.Info("CassandraTask already exists, retrieving its status", "CassandraTask", cTaskKey)
+				if kTask.Status.Datacenters == nil {
+					kTask.Status.Datacenters = make(map[string]cassapi.CassandraTaskStatus)
 				}
-				k8Task.Status.Datacenters[dc.Meta.Name] = actualDcTask.Status
+				kTask.Status.Datacenters[dc.Meta.Name] = actualCTask.Status
 			}
 		}
-		wasComplete := k8Task.Status.CompletionTime != nil
-		k8Task.BuildGlobalStatus()
-		if err = r.Status().Patch(ctx, k8Task, patch); err != nil {
+		wasComplete := kTask.Status.CompletionTime != nil
+		kTask.BuildGlobalStatus()
+		if err = r.Status().Patch(ctx, kTask, patch); err != nil {
 			return ctrl.Result{}, err
 		}
 		// If the status update set a completion time, we want to reconcile again in order to handle the TTL. But
 		// because we configured GenerationChangedPredicate in SetupWithManager(), we ignore our own status updates.
 		// Requeue manually just for this case.
-		if !wasComplete && k8Task.Status.CompletionTime != nil {
+		if !wasComplete && kTask.Status.CompletionTime != nil {
 			return ctrl.Result{Requeue: true}, nil
 		}
 
@@ -208,33 +211,34 @@ func (r *K8ssandraTaskReconciler) getCluster(ctx context.Context, kcKey client.O
 	return kc, true, nil
 }
 
-func (r *K8ssandraTaskReconciler) deleteDcTasks(ctx context.Context, k8Task *api.K8ssandraTask, kc *k8capi.K8ssandraCluster, logger logr.Logger) error {
-	if k8Task.GetConditionStatus(api.JobInvalid) == corev1.ConditionTrue {
+func (r *K8ssandraTaskReconciler) deleteCassandraTasks(ctx context.Context, kTask *api.K8ssandraTask, kc *k8capi.K8ssandraCluster, logger logr.Logger) error {
+	if kTask.GetConditionStatus(api.JobInvalid) == corev1.ConditionTrue {
 		// We never create CassandraTasks when the spec is invalid
 		return nil
 	}
 
-	if dcs, err := filterDcs(kc, k8Task.Spec.Datacenters); err != nil {
+	logger.Info("Deleting CassandraTasks")
+	if dcs, err := filterDcs(kc, kTask.Spec.Datacenters); err != nil {
 		return err
 	} else {
 		for _, dc := range dcs {
 			dcNamespace := utils.FirstNonEmptyString(dc.Meta.Namespace, kc.Namespace)
-			actualDcTask := &cassapi.CassandraTask{
+			actualCTask := &cassapi.CassandraTask{
 				ObjectMeta: metav1.ObjectMeta{
 					Namespace: dcNamespace,
-					Name:      dcTaskName(k8Task, dc.Meta.Name),
+					Name:      cassandraTaskName(kTask, dc.Meta.Name),
 				},
 			}
 			remoteClient, err := r.ClientCache.GetRemoteClient(dc.K8sContext)
 			if err != nil {
 				return err
 			}
-			logger.Info("Deleting DC task", "CassandraTask", actualDcTask)
-			if err := remoteClient.Delete(ctx, actualDcTask); err != nil {
+			logger.Info("Deleting CassandraTask", "CassandraTask", actualCTask)
+			if err := remoteClient.Delete(ctx, actualCTask); err != nil {
 				if !errors.IsNotFound(err) {
 					return fmt.Errorf("deleting CassandraTask %s.%s in context %s: %s",
-						actualDcTask.ObjectMeta.Namespace,
-						actualDcTask.ObjectMeta.Name,
+						actualCTask.ObjectMeta.Namespace,
+						actualCTask.ObjectMeta.Name,
 						dc.K8sContext,
 						err)
 				}
@@ -271,71 +275,74 @@ func filterDcs(kc *k8capi.K8ssandraCluster, dcNames []string) ([]k8capi.Cassandr
 	return dcs, nil
 }
 
-func newDcTask(k8Task *api.K8ssandraTask, namespace string, dcName string) *cassapi.CassandraTask {
-	template := k8Task.Spec.Template
+func newCassandraTask(kTask *api.K8ssandraTask, namespace string, dcName string) *cassapi.CassandraTask {
+	cTemplate := kTask.Spec.Template
 
 	// Never set the TTL on the CassandraTask. We manage it ourselves on the K8ssandraTask, once it expires we'll
 	// cascade-delete the dependent CassandraTasks.
-	template.TTLSecondsAfterFinished = &noTTL
+	cTemplate.TTLSecondsAfterFinished = &noTTL
 
-	dcTask := &cassapi.CassandraTask{
+	return &cassapi.CassandraTask{
 		ObjectMeta: metav1.ObjectMeta{
 			Namespace:   namespace,
-			Name:        dcTaskName(k8Task, dcName),
+			Name:        cassandraTaskName(kTask, dcName),
 			Annotations: map[string]string{},
 			Labels: map[string]string{
 				k8capi.NameLabel:                k8capi.NameLabelValue,
 				k8capi.PartOfLabel:              k8capi.PartOfLabelValue,
 				k8capi.ComponentLabel:           k8capi.ComponentLabelValueCassandra,
 				k8capi.CreatedByLabel:           k8capi.CreatedByLabelValueK8ssandraTaskController,
-				api.K8ssandraTaskNameLabel:      k8Task.Name,
-				api.K8ssandraTaskNamespaceLabel: k8Task.Namespace,
+				api.K8ssandraTaskNameLabel:      kTask.Name,
+				api.K8ssandraTaskNamespaceLabel: kTask.Namespace,
 			},
 		},
 		Spec: cassapi.CassandraTaskSpec{
 			Datacenter:            corev1.ObjectReference{Namespace: namespace, Name: dcName},
-			CassandraTaskTemplate: template,
+			CassandraTaskTemplate: cTemplate,
 		},
 	}
-	return dcTask
 }
 
-func dcTaskName(k8Task *api.K8ssandraTask, dcName string) string {
-	return k8Task.Name + "-" + dcName
+func cassandraTaskName(kTask *api.K8ssandraTask, dcName string) string {
+	return kTask.Name + "-" + dcName
 }
 
-func calculateDeletionTime(k8Task *api.K8ssandraTask) time.Time {
-	if k8Task.Spec.Template.TTLSecondsAfterFinished != nil {
-		if *k8Task.Spec.Template.TTLSecondsAfterFinished == 0 {
+func calculateDeletionTime(kTask *api.K8ssandraTask) time.Time {
+	if kTask.Spec.Template.TTLSecondsAfterFinished != nil {
+		if *kTask.Spec.Template.TTLSecondsAfterFinished == 0 {
 			return time.Time{}
 		}
-		return k8Task.Status.CompletionTime.Add(time.Duration(*k8Task.Spec.Template.TTLSecondsAfterFinished) * time.Second)
+		return kTask.Status.CompletionTime.Add(time.Duration(*kTask.Spec.Template.TTLSecondsAfterFinished) * time.Second)
 	}
-	return k8Task.Status.CompletionTime.Add(defaultTTL)
+	return kTask.Status.CompletionTime.Add(defaultTTL)
 }
 
-func (r *K8ssandraTaskReconciler) recordDcTaskCreated(k8Task *api.K8ssandraTask, desiredDcTask *cassapi.CassandraTask, k8sContext string) {
+func (r *K8ssandraTaskReconciler) recordCassandraTaskCreated(
+	kTask *api.K8ssandraTask,
+	cTask *cassapi.CassandraTask,
+	k8sContext string,
+) {
 	contextInfo := ""
 	if k8sContext != "" {
 		contextInfo = " in context " + k8sContext
 	}
-	r.Recorder.Event(k8Task, "Normal", "CreateCassandraTask",
-		fmt.Sprintf("Created CassandraTask %s.%s%s", desiredDcTask.Namespace, desiredDcTask.Name, contextInfo))
+	r.Recorder.Event(kTask, "Normal", "CreateCassandraTask",
+		fmt.Sprintf("Created CassandraTask %s.%s%s", cTask.Namespace, cTask.Name, contextInfo))
 }
 
 // reportInvalidSpec is called when the user provided an invalid K8ssandraTask spec. A warning event is emitted and the
 // task's status is set to "Invalid".
 func (r *K8ssandraTaskReconciler) reportInvalidSpec(
 	ctx context.Context,
-	k8Task *api.K8ssandraTask,
+	kTask *api.K8ssandraTask,
 	format string,
 	arguments ...interface{},
 ) (ctrl.Result, error) {
-	r.Recorder.Event(k8Task, "Warning", "InvalidSpec", fmt.Sprintf(format, arguments...))
+	r.Recorder.Event(kTask, "Warning", "InvalidSpec", fmt.Sprintf(format, arguments...))
 
-	patch := client.MergeFrom(k8Task.DeepCopy())
-	k8Task.SetCondition(api.JobInvalid, corev1.ConditionTrue)
-	err := r.Status().Patch(ctx, k8Task, patch)
+	patch := client.MergeFrom(kTask.DeepCopy())
+	kTask.SetCondition(api.JobInvalid, corev1.ConditionTrue)
+	err := r.Status().Patch(ctx, kTask, patch)
 	return ctrl.Result{}, err
 }
 
@@ -344,7 +351,7 @@ func (r *K8ssandraTaskReconciler) SetupWithManager(mgr ctrl.Manager, clusters []
 	cb := ctrl.NewControllerManagedBy(mgr).
 		For(&api.K8ssandraTask{}, builder.WithPredicates(predicate.GenerationChangedPredicate{}))
 
-	k8TaskLabelFilter := func(mapObj client.Object) []reconcile.Request {
+	kTaskLabelFilter := func(mapObj client.Object) []reconcile.Request {
 		requests := make([]reconcile.Request, 0)
 
 		taskName := labels.GetLabel(mapObj, api.K8ssandraTaskNameLabel)
@@ -357,11 +364,11 @@ func (r *K8ssandraTaskReconciler) SetupWithManager(mgr ctrl.Manager, clusters []
 	}
 
 	cb = cb.Watches(&source.Kind{Type: &cassapi.CassandraTask{}},
-		handler.EnqueueRequestsFromMapFunc(k8TaskLabelFilter))
+		handler.EnqueueRequestsFromMapFunc(kTaskLabelFilter))
 
 	for _, c := range clusters {
 		cb = cb.Watches(source.NewKindWithCache(&cassapi.CassandraTask{}, c.GetCache()),
-			handler.EnqueueRequestsFromMapFunc(k8TaskLabelFilter))
+			handler.EnqueueRequestsFromMapFunc(kTaskLabelFilter))
 	}
 
 	return cb.Complete(r)
