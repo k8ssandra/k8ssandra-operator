4e410d7273b12f6a5cd34c39bb0c52a8fa8ee6b4
Erik Merkle <erik.merkle@datastax.com>
2023-01-24 10:55:07 -0600

    [fixup] Remove unused functions

--- pkg/utils/deployment.go
+++ pkg/utils/deployment.go
@@ -5,36 +5,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
-// UpdateContainer finds the container with the given name, passes it to f, and then adds it
-// back to the PodTemplateSpec. The Container object is created if necessary before calling
-// f. Only the Name field is initialized.
-func UpdateContainer(d *appsv1.Deployment, name string, f func(c *corev1.Container)) {
-	idx, found := FindContainer(d, name)
-	container := &corev1.Container{}
-
-	if !found {
-		idx = len(d.Spec.Template.Spec.Containers)
-		container = &corev1.Container{Name: name}
-		d.Spec.Template.Spec.Containers = append(d.Spec.Template.Spec.Containers, *container)
-	} else {
-		container = &d.Spec.Template.Spec.Containers[idx]
-	}
-
-	f(container)
-	d.Spec.Template.Spec.Containers[idx] = *container
-}
-
-func AddOrUpdateVolume(deployment *appsv1.Deployment, volume *corev1.Volume) {
-	idx, found := FindVolume(deployment, volume.Name)
-	if !found {
-		// volume doesn't exist, we need to add it
-		deployment.Spec.Template.Spec.Volumes = append(deployment.Spec.Template.Spec.Volumes, *volume)
-	} else {
-		// Overwrite existing volume
-		deployment.Spec.Template.Spec.Volumes[idx] = *volume
-	}
-}
-
 func FindContainer(deployment *appsv1.Deployment, name string) (int, bool) {
 	if deployment != nil {
 		for i, container := range deployment.Spec.Template.Spec.Containers {
