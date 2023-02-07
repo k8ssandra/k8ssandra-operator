508eeba648f2aee9bc8c52a89f409c7dc1196021
Miles-Garnsey <miles.garnsey@datastax.com>
2023-02-07 22:27:02 +1100

    Renaming Try generics.

--- pkg/reconciliation/reconciliation.go
+++ pkg/reconciliation/reconciliation.go
@@ -4,38 +4,47 @@ import (
 	"context"
 	"time"
 
-	k8ssandraapi "github.com/k8ssandra/k8ssandra-operator/apis/k8ssandra/v1alpha1"
 	"github.com/k8ssandra/k8ssandra-operator/pkg/annotations"
-	"github.com/k8ssandra/k8ssandra-operator/pkg/labels"
 	"github.com/k8ssandra/k8ssandra-operator/pkg/result"
 	errors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-type k8sResource interface {
+type Reconcilable[G client.Object] interface {
 	client.Object
 	annotations.Annotated
-	DeepCopy()
-	DeepCopyInto(out *client.Object)
+	DeepCopy() G
+	DeepCopyInto(out G)
+}
+
+type ReconcileWrapper[G Reconcilable[G]] struct {
+	desiredObj G
+	currentObj G
+}
+
+func NewReconcileWrapper[G Reconcilable[G]](desiredObj G) ReconcileWrapper[G] {
+	out := ReconcileWrapper[G]{
+		desiredObj: desiredObj,
+		currentObj: *new(G),
+	}
+	return out
 }
 
 // ReconcileObject takes a desired k8s resource and an empty object of the same type
 // and reconciles it into the desired state, returning a ReconcileResult according to the outcome of the reconciliation.
-func ReconcileObject(ctx context.Context,
-	desiredObj k8sResource,
-	currentObj *client.Object,
-	client client.Client,
+func (wrapper ReconcileWrapper[G]) ReconcileObject(ctx context.Context,
+	kClient client.Client,
 	requeueDelay time.Duration) result.ReconcileResult {
 	objectKey := types.NamespacedName{
-		Name:      desiredObj.GetName(),
-		Namespace: desiredObj.GetNamespace(),
+		Name:      wrapper.desiredObj.GetName(),
+		Namespace: wrapper.desiredObj.GetNamespace(),
 	}
-	annotations.AddHashAnnotation(desiredObj)
-	err := client.Get(ctx, objectKey, *currentObj)
+	annotations.AddHashAnnotation(wrapper.desiredObj)
+	err := kClient.Get(ctx, objectKey, wrapper.currentObj)
 	if err != nil {
 		if errors.IsNotFound(err) {
-			if err := client.Create(ctx, desiredObj); err != nil {
+			if err := kClient.Create(ctx, wrapper.desiredObj); err != nil {
 				return result.Error(err)
 			}
 			return result.RequeueSoon(requeueDelay)
@@ -44,11 +53,11 @@ func ReconcileObject(ctx context.Context,
 		}
 	}
 
-	if !annotations.CompareHashAnnotations(*currentObj, desiredObj) {
-		resourceVersion := (*currentObj).GetResourceVersion()
-		desiredObj.DeepCopyInto(currentObj)
-		(*currentObj).SetResourceVersion(resourceVersion)
-		if err := client.Update(ctx, *currentObj); err != nil {
+	if !annotations.CompareHashAnnotations(wrapper.currentObj, wrapper.desiredObj) {
+		resourceVersion := (wrapper.currentObj).GetResourceVersion()
+		wrapper.desiredObj.DeepCopyInto(wrapper.currentObj)
+		wrapper.currentObj.SetResourceVersion(resourceVersion)
+		if err := kClient.Update(ctx, wrapper.currentObj); err != nil {
 			return result.Error(err)
 		}
 		return result.RequeueSoon(requeueDelay)
@@ -58,16 +67,16 @@ func ReconcileObject(ctx context.Context,
 
 // ReconcileObjectForKluster takes a K8ssandraCluster, a desired object, and an empty object of the same type,
 // and creates an object with all required `part of` labels derived from the K8ssandraCluster.
-func ReconcileObjectForKluster(ctx context.Context,
-	desiredObj k8sResource,
-	currentObj *client.Object,
-	kluster k8ssandraapi.K8ssandraCluster,
-	client client.Client,
-	requeueDelay time.Duration) result.ReconcileResult {
-	kKey := types.NamespacedName{
-		Name:      kluster.Name,
-		Namespace: kluster.Namespace,
-	}
-	labels.IsPartOf(desiredObj, kKey)
-	return ReconcileObject(ctx, desiredObj, currentObj, client, requeueDelay)
-}
+// func ReconcileObjectForKluster(ctx context.Context,
+// 	desiredObj k8sResource,
+// 	currentObj *client.Object,
+// 	kluster k8ssandraapi.K8ssandraCluster,
+// 	client client.Client,
+// 	requeueDelay time.Duration) result.ReconcileResult {
+// 	kKey := types.NamespacedName{
+// 		Name:      kluster.Name,
+// 		Namespace: kluster.Namespace,
+// 	}
+// 	labels.IsPartOf(desiredObj, kKey)
+// 	return ReconcileObject(ctx, desiredObj, currentObj, client, requeueDelay)
+// }
