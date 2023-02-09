da1b011d9334862afbce0a7604bbdf5eae3daf27
Miles-Garnsey <miles.garnsey@datastax.com>
2023-02-09 11:16:08 +1100

    Reconcile k8ssandra's vector configmap using new ConfigMap reconciler.

--- controllers/k8ssandra/vector.go
+++ controllers/k8ssandra/vector.go
@@ -5,9 +5,9 @@ import (
 
 	"github.com/go-logr/logr"
 	k8ssandraapi "github.com/k8ssandra/k8ssandra-operator/apis/k8ssandra/v1alpha1"
-	"github.com/k8ssandra/k8ssandra-operator/pkg/annotations"
 	"github.com/k8ssandra/k8ssandra-operator/pkg/cassandra"
 	k8ssandralabels "github.com/k8ssandra/k8ssandra-operator/pkg/labels"
+	"github.com/k8ssandra/k8ssandra-operator/pkg/reconciliation"
 	"github.com/k8ssandra/k8ssandra-operator/pkg/result"
 	"github.com/k8ssandra/k8ssandra-operator/pkg/telemetry"
 	"github.com/k8ssandra/k8ssandra-operator/pkg/utils"
@@ -37,45 +37,10 @@ func (r *K8ssandraClusterReconciler) reconcileVector(
 		}
 
 		desiredVectorConfigMap := telemetry.BuildVectorAgentConfigMap(namespace, kc.SanitizedName(), dcConfig.Meta.Name, kc.Namespace, toml)
-		annotations.AddHashAnnotation(desiredVectorConfigMap)
 		k8ssandralabels.SetManagedBy(desiredVectorConfigMap, kcKey)
 
 		// Check if the vector config map already exists
-		actualVectorConfigMap := &corev1.ConfigMap{}
-
-		if err := remoteClient.Get(ctx, configMapKey, actualVectorConfigMap); err != nil {
-			if errors.IsNotFound(err) {
-				if err := remoteClient.Create(ctx, desiredVectorConfigMap); err != nil {
-					if errors.IsAlreadyExists(err) {
-						// the read from the local cache didn't catch that the resource was created
-						// already; simply requeue until the cache is up-to-date
-						dcLogger.Info("Vector Agent configuration already exists, requeueing")
-						return result.RequeueSoon(r.DefaultDelay)
-					} else {
-						dcLogger.Error(err, "Failed to create Vector Agent ConfigMap")
-						return result.Error(err)
-					}
-				}
-				// Requeue to ensure the config map can be retrieved
-				return result.RequeueSoon(r.DefaultDelay)
-			} else {
-				dcLogger.Error(err, "Failed to get Vector Agent ConfigMap")
-				return result.Error(err)
-			}
-		}
-
-		actualVectorConfigMap = actualVectorConfigMap.DeepCopy()
-
-		if !annotations.CompareHashAnnotations(actualVectorConfigMap, desiredVectorConfigMap) {
-			resourceVersion := actualVectorConfigMap.GetResourceVersion()
-			desiredVectorConfigMap.DeepCopyInto(actualVectorConfigMap)
-			actualVectorConfigMap.SetResourceVersion(resourceVersion)
-			if err := remoteClient.Update(ctx, actualVectorConfigMap); err != nil {
-				dcLogger.Error(err, "Failed to update Vector Agent ConfigMap resource")
-				return result.Error(err)
-			}
-			return result.RequeueSoon(r.DefaultDelay)
-		}
+		reconciliation.ReconcileConfigMap(ctx, remoteClient, r.DefaultDelay, *desiredVectorConfigMap)
 	} else {
 		if err := deleteConfigMapIfExists(ctx, remoteClient, configMapKey, dcLogger); err != nil {
 			return result.Error(err)
