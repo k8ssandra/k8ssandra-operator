265b8e6db1aeee0aa32ec82d385a9d1199965117
Miles-Garnsey <miles.garnsey@datastax.com>
2023-02-10 17:20:01 +1100

    Missing closing bracket, oops.

--- pkg/reconciliation/configmaps.go
+++ pkg/reconciliation/configmaps.go
@@ -27,10 +27,8 @@ func ReconcileConfigMap(ctx context.Context, kClient client.Client, requeueDelay
 		if errors.IsNotFound(err) {
 			if err := kClient.Create(ctx, &desiredObject); err != nil {
 				if errors.IsAlreadyExists(err) {
-					// the read from the local cache didn't catch that the resource was created
-					// already; simply requeue until the cache is up-to-date
-					dcLogger.Info("Reaper Vector Agent configuration already exists, requeueing")
 					return result.RequeueSoon(requeueDelay)
+				}
 				return result.Error(err)
 			}
 			return result.RequeueSoon(requeueDelay)
