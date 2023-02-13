6adaa0cd32b9566b25fbb87ada5e78c1e586412b
Miles-Garnsey <miles.garnsey@datastax.com>
2023-02-13 17:19:04 +1100

    Remove duplicated unit tests from pkg/telemetry/cassandra_agent_config_test.go.
    These are tested under the new pkg/reconciliation package.

--- pkg/telemetry/cassandra_agent/cassandra_agent_config_test.go
+++ pkg/telemetry/cassandra_agent/cassandra_agent_config_test.go
@@ -15,7 +15,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 )
 
 var (
@@ -110,74 +109,4 @@ func Test_AddStsVolumes(t *testing.T) {
 		SubPath:   "metric-collector.yaml",
 	}
 	assert.Contains(t, dc.Spec.PodTemplateSpec.Spec.Containers[cassContainer].VolumeMounts, expectedVm)
-}
-
-func Test_ReconcileTelemetryAgentConfig_CMCreateSuccess(t *testing.T) {
-	dc := testutils.NewCassandraDatacenter("test-dc", "test-namespace")
-	Cfg.RemoteClient = testutils.NewFakeClientWRestMapper() // Reset the Client
-	recRes := Cfg.ReconcileTelemetryAgentConfig(&dc)
-	assert.True(t, recRes.IsRequeue())
-	actualCm := &corev1.ConfigMap{}
-	err := Cfg.RemoteClient.Get(Cfg.Ctx, types.NamespacedName{Name: Cfg.Kluster.Name + "-" + Cfg.DcName + "-metrics-agent-config", Namespace: Cfg.DcNamespace}, actualCm)
-	assert.NoError(t, err)
-}
-func Test_ReconcileTelemetryAgentConfig_CMCreateFailed(t *testing.T) {
-	dc := testutils.NewCassandraDatacenter("test-dc", "test-namespace")
-	Cfg.RemoteClient = testutils.NewCreateFailingFakeClient() // Reset the Client
-	recRes := Cfg.ReconcileTelemetryAgentConfig(&dc)
-	assert.True(t, recRes.IsError())
-}
-
-func Test_ReconcileTelemetryAgentConfig_CMUpdateSuccess(t *testing.T) {
-	dc := testutils.NewCassandraDatacenter("test-dc", "test-namespace")
-	Cfg.RemoteClient = testutils.NewFakeClientWRestMapper() // Reset the Client
-	// Create an initial ConfigMap with the same name.
-	initialCm, err := Cfg.GetTelemetryAgentConfigMap()
-	if err != nil {
-		assert.Fail(t, "couldn't create ConfigMap")
-	}
-	initialCm.Annotations = make(map[string]string)
-	initialCm.Annotations[k8ssandraapi.ResourceHashAnnotation] = "gobbledegook"
-	initialCm.Data = map[string]string{"gobbledegook": "gobbledegook"}
-	if err := Cfg.RemoteClient.Create(Cfg.Ctx, initialCm); err != nil {
-		assert.Fail(t, "could not create initial ConfigMap")
-	}
-	// Launch reconciliation.
-	Cfg.TelemetrySpec = getExampleTelemetrySpec()
-	recRes := Cfg.ReconcileTelemetryAgentConfig(&dc)
-	assert.True(t, recRes.IsRequeue())
-	// After the update we should see the expected ConfigMap
-	afterUpdateCM := &corev1.ConfigMap{}
-	err = Cfg.RemoteClient.Get(Cfg.Ctx,
-		types.NamespacedName{
-			Name:      Cfg.Kluster.Name + "-" + Cfg.DcName + "-metrics-agent-config",
-			Namespace: Cfg.DcNamespace},
-		afterUpdateCM)
-	assert.NoError(t, err)
-
-	expectedCm := getExpectedConfigMap()
-	assert.NoError(t, err)
-	assert.Equal(t, expectedCm.Data["metric-collector.yaml"], afterUpdateCM.Data["metric-collector.yaml"])
-	assert.Equal(t, expectedCm.Name, afterUpdateCM.Name)
-	assert.Equal(t, expectedCm.Namespace, afterUpdateCM.Namespace)
-}
-
-func Test_ReconcileTelemetryAgentConfig_CMUpdateDone(t *testing.T) {
-	dc := testutils.NewCassandraDatacenter("test-dc", "test-namespace")
-	Cfg.RemoteClient = testutils.NewFakeClientWRestMapper() // Reset the Client
-	// Launch reconciliation.
-	recRes := Cfg.ReconcileTelemetryAgentConfig(&dc)
-	assert.True(t, recRes.IsRequeue())
-	// After the update we should see the expected ConfigMap
-	afterUpdateCM := &corev1.ConfigMap{}
-	err := Cfg.RemoteClient.Get(Cfg.Ctx,
-		types.NamespacedName{
-			Name:      Cfg.Kluster.Name + "-" + Cfg.DcName + "-metrics-agent-config",
-			Namespace: Cfg.DcNamespace,
-		},
-		afterUpdateCM)
-	assert.NoError(t, err)
-	// If we reconcile again, we should move into the Done state.
-	recRes = Cfg.ReconcileTelemetryAgentConfig(&dc)
-	assert.True(t, recRes.IsDone())
 }
