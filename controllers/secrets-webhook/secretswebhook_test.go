23bf3a60322377f3b66a827adc1573ec71e6b0a2
Steven Seidman <steven.seidman@datadoghq.com>
2023-02-23 10:31:19 -0500

    unit tests

--- controllers/secrets-webhook/secretswebhook_test.go
+++ controllers/secrets-webhook/secretswebhook_test.go
@@ -2,13 +2,115 @@ package secrets_webhook
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	admissionv1 "k8s.io/api/admission/v1"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/scheme"
 	log "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
+
+func TestHandleSinceSecretSuccess(t *testing.T) {
+	p := &podSecretsInjector{}
+	d, err := admission.NewDecoder(scheme.Scheme)
+	if err != nil {
+		t.Fatal(err)
+	}
+	p.InjectDecoder(d)
+
+	pod := &corev1.Pod{
+		ObjectMeta: v1.ObjectMeta{
+			Name: "test",
+			Annotations: map[string]string{
+				"k8ssandra.io/inject-secret": `[{"secretName": "mySecret", "path": "/my/secret/path"}]`,
+			},
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{{
+				Name: "test",
+			}},
+		},
+	}
+	pBytes, err := json.Marshal(pod)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req := webhook.AdmissionRequest{AdmissionRequest: admissionv1.AdmissionRequest{
+		UID:       "test123",
+		Name:      "foo",
+		Namespace: "bar",
+		Resource: metav1.GroupVersionResource{
+			Version:  "v1",
+			Resource: "pods",
+		},
+		Operation: "CREATE",
+		Object:    runtime.RawExtension{Raw: pBytes},
+	}}
+
+	resp := p.Handle(context.Background(), req)
+	fmt.Println(fmt.Sprintf("%v", resp))
+	assert.Equal(t, true, resp.AdmissionResponse.Allowed)
+	// 2 patches for addition of volume and volumeMount
+	assert.Equal(t, len(resp.Patches), 2)
+	assert.Equal(t, resp.Patches[0].Operation, "add")
+	assert.Equal(t, resp.Patches[0].Path, "/spec/volumes")
+	assert.Equal(t, resp.Patches[1].Operation, "add")
+	assert.Equal(t, resp.Patches[1].Path, "/spec/containers/0/volumeMounts")
+}
+
+func TestHandleSinceSecretNoPatch(t *testing.T) {
+	p := &podSecretsInjector{}
+	d, err := admission.NewDecoder(scheme.Scheme)
+	if err != nil {
+		t.Fatal(err)
+	}
+	p.InjectDecoder(d)
+
+	pod := &corev1.Pod{
+		ObjectMeta: v1.ObjectMeta{
+			Name: "test",
+			Annotations: map[string]string{
+				"fake-annotation": `fake-val`,
+			},
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{{
+				Name: "test",
+			}},
+		},
+	}
+	pBytes, err := json.Marshal(pod)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req := webhook.AdmissionRequest{AdmissionRequest: admissionv1.AdmissionRequest{
+		UID:       "test123",
+		Name:      "foo",
+		Namespace: "bar",
+		Resource: metav1.GroupVersionResource{
+			Version:  "v1",
+			Resource: "pods",
+		},
+		Operation: "CREATE",
+		Object:    runtime.RawExtension{Raw: pBytes},
+	}}
+
+	resp := p.Handle(context.Background(), req)
+	fmt.Println(fmt.Sprintf("%v", resp))
+	assert.Equal(t, true, resp.AdmissionResponse.Allowed)
+	// no injection annotation, no patch
+	assert.Equal(t, len(resp.Patches), 0)
+}
 
 func TestMutatePodsSingleSecret(t *testing.T) {
 	want := &corev1.Pod{
