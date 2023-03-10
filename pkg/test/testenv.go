8d464ce556c8ad3f9bdad5600e19665a794134a0
Michael Burman <michael.burman@datastax.com>
2023-03-10 12:17:09 +0200

    Fix typo

--- pkg/test/testenv.go
+++ pkg/test/testenv.go
@@ -45,7 +45,7 @@ import (
 const (
 	clustersToCreate          = 3
 	clusterProtoName          = "cluster-%d-%s"
-	cassOperatorVersion       = "1.15.0"
+	cassOperatorVersion       = "v1.15.0"
 	prometheusOperatorVersion = "v0.9.0"
 )
 
