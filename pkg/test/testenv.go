7725008a536c9010b725bf6f30c181d7daa16fc5
Michael Burman <michael.burman@datastax.com>
2023-10-26 08:58:40 +0300

    Update to cass-operator v1.18.1 (#1104)

--- pkg/test/testenv.go
+++ pkg/test/testenv.go
@@ -45,7 +45,7 @@ import (
 const (
 	clustersToCreate          = 3
 	clusterProtoName          = "cluster-%d-%s"
-	cassOperatorVersion       = "v1.17.2"
+	cassOperatorVersion       = "v1.18.1"
 	prometheusOperatorVersion = "v0.9.0"
 )
 
