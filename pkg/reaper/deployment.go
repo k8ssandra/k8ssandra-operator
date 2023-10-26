2d9fa728dbed4b77de82998d03648778110d0bad
Alexander Dejanovski <alex.dejanovski@datastax.com>
2023-10-26 07:58:00 +0200

    Upgrade Reaper to v3.4.0 (#1105)

--- pkg/reaper/deployment.go
+++ pkg/reaper/deployment.go
@@ -24,7 +24,7 @@ import (
 const (
 	DefaultImageRepository = "thelastpickle"
 	DefaultImageName       = "cassandra-reaper"
-	DefaultVersion         = "7162ea3"
+	DefaultVersion         = "3.4.0"
 	// When changing the default version above, please also change the kubebuilder markers in
 	// apis/reaper/v1alpha1/reaper_types.go accordingly.
 
