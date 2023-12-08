11681e017e14c06b67a285885a4b4cd481753955
Alexander Dejanovski <adejanovski@gmail.com>
2023-12-08 16:28:17 +0100

    Add cluster label to Medusa controllers selectors when listing pods

--- pkg/medusa/utils.go
+++ pkg/medusa/utils.go
@@ -8,11 +8,15 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	cassdcapi "github.com/k8ssandra/cass-operator/apis/cassandra/v1beta1"
+	k8ssandraapi "github.com/k8ssandra/k8ssandra-operator/apis/k8ssandra/v1alpha1"
 )
 
 func GetCassandraDatacenterPods(ctx context.Context, cassdc *cassdcapi.CassandraDatacenter, r client.Reader, logger logr.Logger) ([]corev1.Pod, error) {
 	podList := &corev1.PodList{}
-	labels := client.MatchingLabels{cassdcapi.DatacenterLabel: cassdc.DatacenterName()}
+	labels := client.MatchingLabels{
+		k8ssandraapi.K8ssandraClusterNameLabel: cassdcapi.CleanLabelValue(cassdc.Spec.ClusterName),
+		cassdcapi.DatacenterLabel:              cassdc.DatacenterName(),
+	}
 	if err := r.List(ctx, podList, labels, client.InNamespace(cassdc.Namespace)); err != nil {
 		logger.Error(err, "failed to get pods for cassandradatacenter", "CassandraDatacenter", cassdc.DatacenterName())
 		return nil, err
