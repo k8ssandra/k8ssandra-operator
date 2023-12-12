21168ff744c481de2c4fde941622018da7ee06be
Miles-Garnsey <miles.garnsey@datastax.com>
2023-12-12 17:39:29 +1100

    Deal with multiple different cluster names leading to different superuser names.

--- test/e2e/medusa_test.go
+++ test/e2e/medusa_test.go
@@ -199,17 +199,9 @@ func verifyRestoreJobFinished(t *testing.T, ctx context.Context, f *framework.E2
 
 	require.Eventually(func() bool {
 		secret := &corev1.Secret{}
-		err := f.Get(ctx, framework.NewClusterKey(restoreClusterKey.K8sContext, restoreClusterKey.Namespace, "firstcluster-superuser"), secret)
+		err := f.Get(ctx, framework.NewClusterKey(restoreClusterKey.K8sContext, restoreClusterKey.Namespace, cassdcapi.CleanupForKubernetes(dcKey.Name)+"-superuser"), secret)
 		if err != nil {
 			t.Log(err)
-			secretList := &corev1.SecretList{}
-			err = f.List(ctx, framework.NewClusterKey(restoreClusterKey.K8sContext, restoreClusterKey.Namespace, "firstcluster-superuser"), secretList)
-			if err != nil {
-				t.Log(err)
-			}
-			for _, i := range secretList.Items {
-				t.Log(i.Name)
-			}
 			return false
 		}
 		_, exists := secret.Annotations[k8ssandraapi.RefreshAnnotation]
