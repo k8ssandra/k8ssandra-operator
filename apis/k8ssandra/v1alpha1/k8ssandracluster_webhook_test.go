98c371183e92fa4a4ab7045a68a0a879a8a1fc7a
Miles Garnsey <miles.garnsey@datastax.com>
2024-04-03 12:12:39 +1100

    Unit test for this functionality.

--- apis/k8ssandra/v1alpha1/k8ssandracluster_webhook_test.go
+++ apis/k8ssandra/v1alpha1/k8ssandracluster_webhook_test.go
@@ -148,6 +148,7 @@ func TestWebhook(t *testing.T) {
 	t.Run("StsNameTooLong", testStsNameTooLong)
 	t.Run("MedusaPrefixMissing", testMedusaPrefixMissing)
 	t.Run("InvalidDcName", testInvalidDcName)
+	t.Run("MedusaConfigNonLocalNamespace", testMedusaNonLocalNamespace)
 }
 
 func testContextValidation(t *testing.T) {
@@ -464,4 +465,13 @@ func testInvalidDcName(t *testing.T) {
 	err := k8sClient.Create(ctx, clusterWithBadDcName)
 	required.Error(err)
 	required.Contains(err.Error(), "invalid DC name")
+}
+
+func testMedusaNonLocalNamespace(t *testing.T) {
+	required := require.New(t)
+	badCluster := createMinimalClusterObj("medusaconfig-nonlocal", "ns")
+	badCluster.Spec.Medusa.MedusaConfigurationRef.Namespace = "nonlocal-ns"
+	err := badCluster.validateK8ssandraCluster()
+	required.Error(err)
+	required.Contains(err.Error(), "Medusa config must be namespace local")
 }
