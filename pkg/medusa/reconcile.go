bd6f311eda590fad0abc8a4ff5d9c812c76f7ae1
Radovan <rzvoncek@users.noreply.github.com>
2024-05-06 18:55:46 +0300

    Update cassandra-medusa version to 0.21.0 (#1308)

--- pkg/medusa/reconcile.go
+++ pkg/medusa/reconcile.go
@@ -25,7 +25,7 @@ import (
 const (
 	DefaultMedusaImageRepository = "k8ssandra"
 	DefaultMedusaImageName       = "medusa"
-	DefaultMedusaVersion         = "0.20.1"
+	DefaultMedusaVersion         = "0.21.0"
 	DefaultMedusaPort            = 50051
 	DefaultProbeInitialDelay     = 10
 	DefaultProbeTimeout          = 1
