edafeae430ff71cd8b502c5209963a14a9b9367a
Michael Burman <yak@iki.fi>
2024-05-20 12:58:30 +0300

    Add default cache config for cluster-scope

--- main.go
+++ main.go
@@ -40,7 +40,6 @@ import (
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
-	"k8s.io/client-go/rest"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
@@ -142,22 +141,21 @@ func main() {
 		},
 	}
 
-	// Add support for MultiNamespace set in WATCH_NAMESPACE (e.g ns1,ns2)
-	if watchNamespace != "" {
-		nsConfig := make(map[string]cache.Config)
-		if strings.Contains(watchNamespace, ",") {
-			for _, i := range strings.Split(watchNamespace, ",") {
-				nsConfig[i] = cache.Config{}
-			}
+	options.Cache = cache.Options{
+		DefaultNamespaces: map[string]cache.Config{},
+	}
 
-		} else {
-			nsConfig[watchNamespace] = cache.Config{}
-			setupLog.Info("watch namespace configured", "namespace", watchNamespace)
+	// Add support for MultiNamespace set in WATCH_NAMESPACE (e.g ns1,ns2)
+	if strings.Contains(watchNamespace, ",") {
+		setupLog.Info("manager set up with multiple namespaces", "namespaces", watchNamespace)
+		// configure cluster-scoped with MultiNamespacedCacheBuilder
+		namespaces := strings.Split(watchNamespace, ",")
+		for _, namespace := range namespaces {
+			options.Cache.DefaultNamespaces[namespace] = cache.Config{}
 		}
-		options.NewCache = func(config *rest.Config, opts cache.Options) (cache.Cache, error) {
-			opts.DefaultNamespaces = nsConfig
-			return cache.New(config, opts)
-		}
+	} else if watchNamespace != "" {
+		setupLog.Info("watch namespace configured", "namespace", watchNamespace)
+		options.Cache.DefaultNamespaces[watchNamespace] = cache.Config{}
 	}
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), options)
