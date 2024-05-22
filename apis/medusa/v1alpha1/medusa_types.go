ba9d08f93182a9a934c1427dff9a59b81f2fc5be
Eric Smalling <eric.smalling@chainguard.dev>
2024-05-22 06:36:05 -0500

    Add ability to disable medusa-purge cron

--- apis/medusa/v1alpha1/medusa_types.go
+++ apis/medusa/v1alpha1/medusa_types.go
@@ -181,4 +181,10 @@ type MedusaClusterTemplate struct {
 	// Define the liveness probe settings to use for the Medusa containers.
 	// +optional
 	LivenessProbe *corev1.Probe `json:"livenessProbe,omitempty"`
+
+	// Should medusa purge backups nightly or not
+	// Defaults to true.
+	// +kubebuilder:default=true
+	// +optional
+	PurgeBackups bool `json:"purgeBackups,omitempty"`
 }
