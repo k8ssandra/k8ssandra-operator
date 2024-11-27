5940bccb95b4dcbb285279f6d196596ccdd7eff7
Alexander Dejanovski <alex.dejanovski@datastax.com>
2024-11-27 11:03:57 +0100

    Upgrade Reaper to 3.7.0 (#1459)

--- apis/reaper/v1alpha1/reaper_types.go
+++ apis/reaper/v1alpha1/reaper_types.go
@@ -85,10 +85,10 @@ type ReaperTemplate struct {
 	SecretsProvider string `json:"secretsProvider,omitempty"`
 
 	// The image to use for the Reaper pod main container.
-	// The default is "thelastpickle/cassandra-reaper:3.6.1".
+	// The default is "thelastpickle/cassandra-reaper:3.7.0".
 	// +optional
 	// TODO: update with real release version.
-	// +kubebuilder:default={repository:"thelastpickle",name:"cassandra-reaper",tag:"3.6.1"}
+	// +kubebuilder:default={repository:"thelastpickle",name:"cassandra-reaper",tag:"3.7.0"}
 	ContainerImage *images.Image `json:"containerImage,omitempty"`
 
 	// Deprecated: The main container image will be used for the init container as well.
