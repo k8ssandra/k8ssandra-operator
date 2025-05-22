c3a3cbac6610fc17429c6931e27ccbef667fb6a8
Michael Burman <yak@iki.fi>
2025-05-22 18:43:39 +0300

    Update to kustomize v5.6.0 and modify test framework to work with it

--- test/framework/e2e_framework.go
+++ test/framework/e2e_framework.go
@@ -222,10 +222,6 @@ replacements:
     fieldPaths:
       - metadata.namespace
   - select:
-      kind: ClusterRoleBinding
-    fieldPaths:
-      - subjects.0.namespace
-  - select:
       name: cass-operator-validating-webhook-configuration
       kind: ValidatingWebhookConfiguration
     fieldPaths:
