0f95fdd729a99a3245cc54ae2386957396f5c24f
Michael Burman <michael.burman@datastax.com>
2025-05-23 07:49:18 +0300

    Update to kustomize v5.6.0 and modify test framework to work with it (#1540)
    * Update to kustomize v5.6.0 and modify test framework to work with it
    
    * Remove from other deployment model also

--- test/framework/e2e_framework.go
+++ test/framework/e2e_framework.go
@@ -160,10 +160,6 @@ replacements:
     fieldPaths:
       - metadata.namespace
   - select:
-      kind: ClusterRoleBinding
-    fieldPaths:
-      - subjects.0.namespace
-  - select:
       name: cass-operator-validating-webhook-configuration
       kind: ValidatingWebhookConfiguration
     fieldPaths:
@@ -221,10 +217,6 @@ replacements:
       namespace: k8ssandra-operator
     fieldPaths:
       - metadata.namespace
-  - select:
-      kind: ClusterRoleBinding
-    fieldPaths:
-      - subjects.0.namespace
   - select:
       name: cass-operator-validating-webhook-configuration
       kind: ValidatingWebhookConfiguration
