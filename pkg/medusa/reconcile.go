9a776fa0edde514cc35f6bf2d7ee0f7cf90e32e5
Radovan Zvoncek <rzvoncek@gmail.com>
2025-06-16 12:14:28 +0300

    Bump default Medusa image to 0.24.1

--- pkg/medusa/reconcile.go
+++ pkg/medusa/reconcile.go
@@ -25,7 +25,7 @@ import (
 const (
 	DefaultMedusaImageRepository = "k8ssandra"
 	DefaultMedusaImageName       = "medusa"
-	DefaultMedusaVersion         = "0.24.0"
+	DefaultMedusaVersion         = "0.24.1"
 	DefaultMedusaPort            = 50051
 	DefaultProbeInitialDelay     = 10
 	DefaultProbeTimeout          = 1
