8a995265a192c0a625426d826368218729810431
Alexander Dejanovski <alex.dejanovski@datastax.com>
2025-08-05 15:43:23 +0200

    Fix failing tests

--- test/e2e/cluster_scope_test.go
+++ test/e2e/cluster_scope_test.go
@@ -25,6 +25,7 @@ func multiDcMultiCluster(t *testing.T, ctx context.Context, klusterNamespace str
 	require.NoError(err, "failed to get K8ssandraCluster in operatorNamespace %s", klusterNamespace)
 
 	dc1Key := framework.ClusterKey{K8sContext: f.DataPlaneContexts[0], NamespacedName: types.NamespacedName{Namespace: dc1Namespace, Name: "dc1"}}
+	dc1Prefix := DcPrefix(t, f, dc1Key)
 	checkDatacenterReady(t, ctx, dc1Key, f)
 	verifyBucketKeyPresent(t, f, ctx, k8ssandra, klusterNamespace, dc1Key.K8sContext, multiClusterBucketSecretName)
 	verifyBucketKeyPresent(t, f, ctx, k8ssandra, dc1Key.Namespace, dc1Key.K8sContext, multiClusterBucketSecretName)
@@ -45,6 +46,7 @@ func multiDcMultiCluster(t *testing.T, ctx context.Context, klusterNamespace str
 	}, polling.k8ssandraClusterStatus.timeout, polling.k8ssandraClusterStatus.interval, "timed out waiting for K8ssandraCluster status to get updated")
 
 	dc2Key := framework.ClusterKey{K8sContext: f.DataPlaneContexts[1], NamespacedName: types.NamespacedName{Namespace: dc2Namespace, Name: "dc2"}}
+	dc2Prefix := DcPrefix(t, f, dc2Key)
 	checkDatacenterReady(t, ctx, dc2Key, f)
 	verifyBucketKeyPresent(t, f, ctx, k8ssandra, dc2Namespace, dc2Key.K8sContext, multiClusterBucketSecretName)
 
@@ -80,20 +82,18 @@ func multiDcMultiCluster(t *testing.T, ctx context.Context, klusterNamespace str
 	require.NoError(err, "failed to retrieve database credentials")
 
 	t.Log("check that nodes in dc1 see nodes in dc2")
-	pod := DcPrefix(t, f, dc1Key) + "-rack1-sts-0"
+	pod := dc1Prefix + "-rack1-sts-0"
 	count := 4
 	checkNodeToolStatus(t, f, f.DataPlaneContexts[0], dc1Namespace, pod, count, 0, "-u", username, "-pw", password)
 
 	t.Log("check nodes in dc2 see nodes in dc1")
-	pod = DcPrefix(t, f, dc2Key) + "-rack1-sts-0"
+	pod = dc2Prefix + "-rack1-sts-0"
 	checkNodeToolStatus(t, f, f.DataPlaneContexts[1], dc2Namespace, pod, count, 0, "-u", username, "-pw", password)
 
 	t.Log("check that cluster was registered in Reaper")
 	reaperKey := framework.ClusterKey{K8sContext: f.ControlPlaneContext, NamespacedName: types.NamespacedName{Namespace: reaperNamespace, Name: "reaper1"}}
-	dcKey := framework.ClusterKey{K8sContext: f.DataPlaneContexts[0], NamespacedName: types.NamespacedName{Namespace: dc1Namespace, Name: "dc1"}}
-	dcPrefix := DcPrefix(t, f, dcKey)
 	checkReaperReady(t, f, ctx, reaperKey)
-	createKeyspaceAndTable(t, f, ctx, f.DataPlaneContexts[0], dc1Namespace, k8ssandra.Name, dcPrefix+"-rack1-sts-0", "test_ks", "test_table", 2)
+	createKeyspaceAndTable(t, f, ctx, f.DataPlaneContexts[0], dc1Namespace, k8ssandra.Name, dc1Prefix+"-rack1-sts-0", "test_ks", "test_table", 2)
 
 	t.Log("deploying Reaper ingress routes in context", f.ControlPlaneContext)
 	reaperRestHostAndPort := ingressConfigs[f.ControlPlaneContext].ReaperRest
@@ -105,7 +105,7 @@ func multiDcMultiCluster(t *testing.T, ctx context.Context, klusterNamespace str
 		t.Log("test Reaper API in context", f.ControlPlaneContext)
 		secretKey := framework.ClusterKey{K8sContext: f.ControlPlaneContext, NamespacedName: types.NamespacedName{Namespace: k8ssandra.Namespace, Name: "reaper-ui-secret"}}
 		username, password := retrieveCredentials(t, f, ctx, secretKey)
-		testReaperApi(t, ctx, f.ControlPlaneContext, DcClusterName(t, f, dcKey), "test_ks", username, password)
+		testReaperApi(t, ctx, f.ControlPlaneContext, DcClusterName(t, f, dc1Key), "test_ks", username, password)
 	})
 
 	// Delete the K8ssandraCluster
@@ -122,11 +122,11 @@ func multiDcMultiCluster(t *testing.T, ctx context.Context, klusterNamespace str
 
 	// Check that the secrets were deleted
 	t.Log("checking that the secrets were deleted")
-	reaper1SecretKey := framework.ClusterKey{K8sContext: f.DataPlaneContexts[0], NamespacedName: types.NamespacedName{Namespace: k8ssandra.Namespace, Name: DcPrefix(t, f, dc1Key) + "-reaper"}}
+	reaper1SecretKey := framework.ClusterKey{K8sContext: f.DataPlaneContexts[0], NamespacedName: types.NamespacedName{Namespace: k8ssandra.Namespace, Name: dc1Prefix + "-reaper"}}
 	err = f.Get(ctx, reaper1SecretKey, &corev1.Secret{})
 	require.True(errors.IsNotFound(err), "Reaper UI secret should not exist in dc1")
 
-	reaper2SecretKey := framework.ClusterKey{K8sContext: f.DataPlaneContexts[1], NamespacedName: types.NamespacedName{Namespace: k8ssandra.Namespace, Name: DcPrefix(t, f, dc2Key) + "-reaper"}}
+	reaper2SecretKey := framework.ClusterKey{K8sContext: f.DataPlaneContexts[1], NamespacedName: types.NamespacedName{Namespace: k8ssandra.Namespace, Name: dc2Prefix + "-reaper"}}
 	err = f.Get(ctx, reaper2SecretKey, &corev1.Secret{})
 	require.True(errors.IsNotFound(err), "Reaper UI secret should not exist in dc2")
 
