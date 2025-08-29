43066c2087176de0ad13a7d09f899d49de502816
Michael Burman <yak@iki.fi>
2025-08-29 17:06:18 +0300

    Fix per_node_config mount call

--- controllers/k8ssandra/per_node_config.go
+++ controllers/k8ssandra/per_node_config.go
@@ -161,7 +161,7 @@ func (r *K8ssandraClusterReconciler) reconcileUserProvidedPerNodeConfiguration(
 
 		perNodeConfigMapHash := utils.DeepHashString(actualPerNodeConfig)
 		annotations.AddAnnotation(&dcConfig.PodTemplateSpec, k8ssandraapi.PerNodeConfigHashAnnotation, perNodeConfigMapHash)
-		nodeconfig.MountPerNodeConfig(dcConfig)
+		nodeconfig.MountPerNodeConfig(dcConfig, r.ImageRegistry)
 		dcLogger.Info("Mounted per-node configuration")
 
 	} else {
