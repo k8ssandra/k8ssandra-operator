d20087df3f99292d224a8dabea73e0dc991f2b6a
Radovan Zvoncek <rzvoncek@gmail.com>
2026-08-04 15:48:15 +0300

    Allow setting Medusa's chunk size

--- apis/medusa/v1alpha1/medusa_types.go
+++ apis/medusa/v1alpha1/medusa_types.go
@@ -86,6 +86,11 @@ type Storage struct {
 	// +optional
 	MultiPartUploadThreshold int `json:"multiPartUploadThreshold,omitempty"`
 
+	// Chunk size in bytes for multipart uploads.
+	// Defaults to 0 (medusa uses its own default).
+	// +optional
+	MultiPartChunkSize int `json:"multiPartChunkSize,omitempty"`
+
 	// Host to connect to for the storage backend.
 	// +optional
 	Host string `json:"host,omitempty"`
